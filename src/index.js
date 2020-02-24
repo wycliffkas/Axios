efe1a5c3757ba72284b1fe9e1ca61c7df541965b
w4wycliff <w4wycliff@gmail.com>
2020-02-24 15:46:03 +0300

    adding axios interceptors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+//setting axios global defaults
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com"
+
+axios.interceptors.request.use(request => {
+    console.log("request", request);
+    //can edit the request
+    return request;
+}, error => {
+    console.log(error);
+    //error for request sending eg internet connection
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    console.log("response", response);
+    return response;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
