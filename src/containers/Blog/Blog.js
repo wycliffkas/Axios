a9ee8adb03cc3104535763003656dc24f123e707
w4wycliff <w4wycliff@gmail.com>
2020-03-03 22:33:19 +0300

    lazy loading routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,60 +1,65 @@
-import React, { Component } from "react";
-import axios from "axios";
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
+import React, { Component, Suspense } from "react";
+// import axios from 'axios';
+import { Route, NavLink, Switch } from "react-router-dom";
+
 import "./Blog.css";
+import Posts from "./Posts/Posts";
+import NewPost from "./NewPost/NewPost";
+
+const Post = React.lazy(() => import("./Posts/Posts"));
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  componentDidMount() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/posts")
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: "Wycliff"
-          };
-        });
-        this.setState({ posts: updatedPosts });
-      })
-      .catch(error => this.setState({ error: true }));
-  }
-
-  postSelectedHandler = id => {
-    this.setState({ selectedPostId: id });
-  };
-
   render() {
-    let posts = (
-      <p style={{ textAlign: "center" }}>Something wrong has happened!</p>
-    );
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => (
-        <Post
-          clicked={() => this.postSelectedHandler(post.id)}
-          key={post.id}
-          title={post.title}
-          author={post.author}
-        />
-      ));
-    }
     return (
-      <div>
-        <section className="Posts">{posts}</section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
+                  New Post
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+        <Switch>
+          {/* {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null} */}
+          <Route path="/new-post" component={NewPost} />
+          <Route path="/new-post" component={Posts} />
+          {/* <Route
+            path="/posts"
+            render={() => (
+              <Suspense fallback={<div>Loading</div>}>
+                <Post />
+              </Suspense>
+            )}
+          /> */}
+          <Route render={() => <h1>Not found</h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
+          {/* <Route path="/" component={Posts} /> */}
+        </Switch>
       </div>
     );
   }
