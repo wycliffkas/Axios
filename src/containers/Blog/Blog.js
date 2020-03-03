245db0b138eaa265bbe17c2343d8eb751b1d8510
w4wycliff <w4wycliff@gmail.com>
2020-03-03 22:08:01 +0300

    refactor

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,77 +1,63 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
 
-    state = {
-        posts: [],
-<<<<<<< HEAD
-        selectedPostId: null
-    }
-    
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-=======
-        selectedPostId: null,
-        error: false
-    }
-    
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/postssss')
->>>>>>> Initial commit
-        .then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post, 
-                    author: 'Wycliff'
-                }
-            })
-            this.setState({posts: updatedPosts})}
-<<<<<<< HEAD
-        );
-=======
-        )
-        .catch(error => this.setState({error: true}));
->>>>>>> Initial commit
-    }
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Wycliff"
+          };
+        });
+        this.setState({ posts: updatedPosts });
+      })
+      .catch(error => this.setState({ error: true }));
+  }
 
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostId: id});
-    }
-    
-    render () {
-<<<<<<< HEAD
-        const posts = this.state.posts.map(post => (
-            <Post  clicked={() => this.postSelectedHandler(post.id)} key={post.id} title={post.title} author={post.author}/>
-        ))
+  postSelectedHandler = id => {
+    this.setState({ selectedPostId: id });
+  };
 
-=======
-        let posts = <p style={{textAlign: "center"}}>Something wrong has happened!</p>
-        if(!this.state.error) {
-            posts = this.state.posts.map(post => (
-                <Post  clicked={() => this.postSelectedHandler(post.id)} key={post.id} title={post.title} author={post.author}/>
-            ))
-        }
->>>>>>> Initial commit
-        return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  render() {
+    let posts = (
+      <p style={{ textAlign: "center" }}>Something wrong has happened!</p>
+    );
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => (
+        <Post
+          clicked={() => this.postSelectedHandler(post.id)}
+          key={post.id}
+          title={post.title}
+          author={post.author}
+        />
+      ));
     }
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost id={this.state.selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
